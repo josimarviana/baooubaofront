8440ad0d8bdbe5a6f855d28e79b5b5c428075ed1
Cristian Alves Silva <cristianalvessilvak@gmail.com>
2024-03-21 21:59:12 -0300

    Add confirm password field, and show and hide password

---
+++ assets/js/auth/signup-assets.js
@@ -0,0 +1,14 @@
+document.getElementById('show-password').addEventListener('click', function () {
+    var passwordField = document.getElementById('signup-password');
+    var passwordIcon = document.getElementById('show-pass-icon');
+    var passwordFieldType = passwordField.getAttribute('type');
+    if (passwordFieldType === 'password') {
+        passwordField.setAttribute('type', 'text');
+        passwordIcon.classList.remove('fa-eye-slash');
+        passwordIcon.classList.add('fa-eye');
+    } else {
+        passwordField.setAttribute('type', 'password');
+        passwordIcon.classList.remove('fa-eye');
+        passwordIcon.classList.add('fa-eye-slash');
+    }
+});
