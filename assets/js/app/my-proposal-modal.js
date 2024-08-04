0ae78b6df7bcc82bc6aa588d4dfe4179a419ff15
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-08-04 17:27:30 -0300

    Visualizando minhas propostas, cluido

--- assets/js/app/my-proposal-modal.js
+++ assets/js/app/my-proposal-modal.js
@@ -6,23 +6,95 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "../errors/404.html"; // Redirecionar para a página de login
     return; // Interrompe a execução da função
   }
-});
-proposalModal.addEventListener("show.bs.modal", async function (event) {
-  const button = event.relatedTarget;
-  const id = button.getAttribute("data-proposal-id");
 
-  const apiUrl = `https://apibaoounao.iftmparacatu.app.br/proposal/${id}`;
+  proposalModal.addEventListener("show.bs.modal", async function (event) {
+    const button = event.relatedTarget;
+    const id = button.getAttribute("data-proposal-id");
 
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+    const apiUrl = `https://apibaoounao.iftmparacatu.app.br/proposal/${id}`;
+
+    try {
+      const response = await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      // Atualizar o modal com os dados dinâmicos obtidos da API
+      const modalTitle = proposalModal.querySelector("#proposalModalLabel");
+      const proposalTitle = proposalModal.querySelector("#proposal-title");
+      const proposalDescription = proposalModal.querySelector(
+        "#proposal-description"
+      );
+      const proposalLikes = proposalModal.querySelector("#proposal-likes");
+      const proposalSituation = proposalModal.querySelector(
+        "#proposal-situation"
+      );
+      const proposalAuthor = proposalModal.querySelector("#proposal-author");
+      const proposalCategory =
+        proposalModal.querySelector("#proposal-category");
+      const proposalImage = proposalModal.querySelector("#proposal-image");
+
+      const defaultImageUrl =
+        "https://plus.unsplash.com/premium_photo-1676750395664-3ac0783ae2c2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+      const imageData = data.image;
+      if (imageData) {
+        const imageUrl = `data:image/jpeg;base64,${imageData}`;
+        proposalImage.src = imageUrl;
+      } else {
+        proposalImage.src = defaultImageUrl;
+      }
+
+      proposalImage.onload = () => {
+        console.log("Imagem carregada com sucesso!");
+      };
+
+      proposalImage.onerror = (error) => {
+        console.error("Erro ao carregar a imagem:", error);
+      };
+      console.log(data);
+      modalTitle.textContent =
+        "Criada em: " + new Date(data.createdAt).toLocaleDateString();
+      proposalTitle.textContent = data.title;
+      proposalDescription.textContent = data.description;
+      proposalLikes.textContent = data.likes;
+
+      const situation = data.situation;
+      switch (situation) {
+        case "OPEN_FOR_VOTING":
+          proposalSituation.textContent = "Em votação";
+          proposalSituation.classList.add("text-bg-info");
+          break;
+        case "FORWARDED_TO_BOARD":
+          proposalSituation.textContent = "Encaminhado para o conselho";
+          proposalSituation.classList.add("text-bg-success");
+          break;
+        case "APPROVED":
+          proposalSituation.textContent = "Deferido";
+          proposalSituation.classList.add("text-bg-primary");
+          break;
+        case "DENIED":
+          proposalSituation.textContent = "Indeferido";
+          proposalSituation.classList.add("text-bg-danger");
+          break;
+        case "PENDING_MODERATION":
+          proposalSituation.textContent = "Em análise";
+          proposalSituation.classList.add("text-bg-warning");
+          break;
+        default:
+          proposalSituation.textContent = "";
+          proposalSituation.classList.add("bg-light");
+          break;
+      }
+
+      proposalCategory.textContent = data.category;
+    } catch (error) {
+      console.error("Erro ao obter dados da API:", error);
     }
-
-    const data = await response.json();
-  } catch (Error) {}
+  });
 });
--- assets/js/app/my-proposals.js
+++ assets/js/app/my-proposals.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     data.forEach((item) => {
       console.log(item);
       const newCard = document.createElement("div");
-      newCard.className = "col-12 col-lg-4";
+     newCard.className = "col-12 col-lg-4";
       newCard.innerHTML = `
       <div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
         <div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
@@ -48,6 +48,8 @@ document.addEventListener("DOMContentLoaded", function () {
                   </svg>
                 </div>
               </div>
+            
+
               <div class="col-auto">
                 <h4 class="app-card-title">${item.title}</h4>
               </div>
@@ -57,6 +59,50 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="intro">
               ${item.description}
             </div>
+            <div class="app-card-actions">
+                                    <div class="dropdown">
+                                        <div class="dropdown-toggle no-toggle-arrow" data-bs-toggle="dropdown"
+                                            aria-expanded="false">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16"
+                                                class="bi bi-three-dots-vertical" fill="currentColor"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                                            </svg>
+                                        </div>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#" class="dropdown-item"><svg
+                                                width="1em"
+                                                height="1em"
+                                                viewBox="0 0 16 16"
+                                                class="bi bi-pencil me-2"
+                                                fill="currentColor"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                              >
+                                                <path
+                                                  fill-rule="evenodd"
+                                                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
+                                                /></svg
+                                              > Editar</a></li>
+                                            <li><a href="#" class="dropdown-item"><svg
+                                                width="1em"
+                                                height="1em"
+                                                viewBox="0 0 16 16"
+                                                class="bi bi-trash me-2"
+                                                fill="currentColor"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                              >
+                                                <path
+                                                  d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+                                                />
+                                                <path
+                                                  fill-rule="evenodd"
+                                                  d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                                                /></svg
+                                              > Excluir</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
           </div>
           <div class="app-card-footer p-4 mt-auto">
             <a class="btn app-btn-secondary" href="#" data-bs-toggle="modal" data-bs-target="#proposalModal" data-proposal-id="${item.id}">Visualizar</a>
