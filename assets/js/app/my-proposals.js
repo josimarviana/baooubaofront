2ae9d12776f4ae739c35b2ae9b1adbd8d8296a82
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-08-09 15:13:04 -0300

    Justify text in proposal cards

--- assets/js/app/my-proposals.js
+++ assets/js/app/my-proposals.js
@@ -37,21 +37,16 @@ document.addEventListener("DOMContentLoaded", function () {
         <div class="app-card app-card-basic d-flex flex-column align-items-start shadow-sm">
                             <div class="app-card-header p-3 border-bottom-0">
                                 <div class="row align-items-center gx-3">
-                                    <div>
-                                        <div class="app-icon-holder">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-code-square"
-                                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd"
-                                                    d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <h4 class="app-card-title">${item.title}</h4>
-                                    </div>
-                                </div>
+              <div class="d-flex align-items-center">
+                <div class="app-icon-holder me-2">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-code-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                    <path fill-rule="evenodd" d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
+                  </svg>
+                </div>
+                <h4 class="app-card-title mb-0">${item.title}</h4>
+              </div>
+            </div>
                             </div>
                             <div class="app-card-body px-4">
                                 <div class="intro">${item.description}
