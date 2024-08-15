95fae73ccf3e5d22fb12cbac284100455a76d586
Cristian Alves Silva <cristianalvessilvak@gmail.com>
2024-08-14 22:27:03 -0300

    View video if exists on all modal for viewing proposals

--- assets/js/app/modal.js
+++ assets/js/app/modal.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const proposalModal = document.getElementById("proposalModal");
+
   if (!sessionStorage.getItem("jwt")) {
     console.error("Usuário não autenticado. Redirecionando para o login.");
     window.location.href = "../errors/404.html";
@@ -42,26 +43,44 @@ document.addEventListener("DOMContentLoaded", function () {
   function updateModalContent(data) {
     const modalTitle = proposalModal.querySelector("#proposalModalLabel");
     const proposalTitle = proposalModal.querySelector("#proposal-title");
-    const proposalDescription = proposalModal.querySelector(
-      "#proposal-description"
-    );
+    const proposalDescription = proposalModal.querySelector("#proposal-description");
     const proposalLikes = proposalModal.querySelector("#proposal-likes");
     const proposalAuthor = proposalModal.querySelector("#proposal-author");
     const proposalCategory = proposalModal.querySelector("#proposal-category");
-    const proposalImage = proposalModal.querySelector("#proposal-image");
-    const defaultImageUrl = "../../../assets/images/BaoOuNao.png";
+    const imageContainer = proposalModal.querySelector("#image-container");
+    const proposalVideo = proposalModal.querySelector("#proposal-video");
 
-    modalTitle.textContent =
-      "Criada em: " + new Date(data.createdAt).toLocaleDateString();
+    // Configuração da imagem
+    const imageData = data.image;
+    if (imageData) {
+      const imageUrl = `data:image/jpeg;base64,${imageData}`;
+      const proposalImage = document.createElement("img");
+      proposalImage.src = imageUrl;
+      proposalImage.alt = "Imagem descritiva da proposta";
+      proposalImage.classList.add("img-fluid", "rounded");
+
+      imageContainer.innerHTML = ""; // Limpa o conteúdo do contêiner
+      imageContainer.appendChild(proposalImage); // Adiciona a nova imagem
+      imageContainer.style.display = "flex"; // Mostra o contêiner da imagem
+    } else {
+      imageContainer.style.display = "none"; // Oculta o contêiner da imagem
+    }
+
+    modalTitle.textContent = "Criada em: " + new Date(data.createdAt).toLocaleDateString();
     proposalTitle.textContent = data.title;
     proposalDescription.textContent = data.description;
     proposalLikes.textContent = data.likes;
     proposalAuthor.textContent = "Autor: " + data.author;
     proposalCategory.textContent = data.category;
 
-    proposalImage.src = data.image ?
-      `data:image/jpeg;base64,${data.image}` :
-      defaultImageUrl;
+    const videoUrl = data.videoUrl;
+    if (videoUrl) {
+      const embedUrl = convertToEmbedUrl(videoUrl);
+      proposalVideo.innerHTML = `<iframe width="100%" src="${embedUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+      proposalVideo.style.display = "flex";
+    } else {
+      proposalVideo.style.display = "none";
+    }
   }
 
   async function checkIfUserHasVoted(proposalId) {
@@ -97,7 +116,6 @@ document.addEventListener("DOMContentLoaded", function () {
       voteButton.innerHTML = '<i class="fa-regular fa-thumbs-up"></i> Bão';
     }
   }
-
 
   proposalModal.addEventListener("click", async function (event) {
     if (event.target && event.target.matches("#btn-vote")) {
@@ -137,10 +155,8 @@ document.addEventListener("DOMContentLoaded", function () {
       if (!response.ok) {
         const errorText = await response.text();
         let errorMessage = "Erro desconhecido";
-        o
 
         try {
-
           const errorData = JSON.parse(errorText);
           errorMessage = errorData.mensagem || "Erro desconhecido";
         } catch (jsonError) {
@@ -148,12 +164,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         console.error("Erro ao processar votação:", errorText);
-
-
         showToast(`Erro: ${errorMessage}`, "error");
         return;
       }
-
 
       showToast(
         hasVoted ?
@@ -161,11 +174,8 @@ document.addEventListener("DOMContentLoaded", function () {
         "Voto registrado com sucesso!"
       );
 
-
       hasVoted = !hasVoted;
-      const voteButton = proposalModal.querySelector("#btn-vote");
-      updateVoteButton(voteButton);
-
+      updateVoteButton();
 
       setTimeout(() => {
         const modal = bootstrap.Modal.getInstance(proposalModal);
@@ -184,12 +194,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const toastElement = document.getElementById("confirmationToast");
     const toastBody = document.getElementById("toast-body");
 
-
     toastBody.textContent = message;
 
-
     toastElement.classList.remove("text-success", "text-danger");
-
 
     if (type === "success") {
       toastElement.classList.add("text-primary");
@@ -212,5 +219,11 @@ document.addEventListener("DOMContentLoaded", function () {
     } catch (error) {
       console.error("Erro ao atualizar o modal após a votação:", error);
     }
+  }
+
+  // Função para converter URL do YouTube para formato embed
+  function convertToEmbedUrl(url) {
+    const videoId = new URL(url).searchParams.get("v");
+    return videoId ? `https://www.youtube.com/embed/${videoId}` : "";
   }
 });
