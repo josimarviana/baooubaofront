99a8e4a03298b1e7cd6027955ae05b30cf9f337e
Cristian Alves Silva <cristianalvessilvak@gmail.com>
2024-08-28 21:50:46 -0300

    Add modal forgot password

--- assets/js/auth/login-assets.js
+++ assets/js/auth/login-assets.js
@@ -1,11 +1,13 @@
-import config from '../environments/config.js';
-import showToast from '../app/toast.js';
+import config from "../environments/config.js";
+import showToast from "../app/toast.js";
+
 const apiUrl = config.api + "/user/login";
 
 document.getElementById("show-password").addEventListener("click", function () {
-  var passwordField = document.getElementById("login-password");
-  var passwordIcon = document.getElementById("show-pass-icon");
-  var passwordFieldType = passwordField.getAttribute("type");
+  const passwordField = document.getElementById("login-password");
+  const passwordIcon = document.getElementById("show-pass-icon");
+  const passwordFieldType = passwordField.getAttribute("type");
+
   if (passwordFieldType === "password") {
     passwordField.setAttribute("type", "text");
     passwordIcon.classList.remove("fa-eye-slash");
@@ -19,29 +21,26 @@ document.getElementById("show-password").addEventListener("click", function () {
 
 const loginForm = document.querySelector("#loginForm");
 
-
 loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const email = document.querySelector("#login-email").value;
   const password = document.querySelector("#login-password").value;
-  const data = {
-    email,
-    password,
-  };
+  const data = { email, password };
 
   try {
     const response = await fetch(apiUrl, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
 
     if (!response.ok) {
       const errorResponse = await response.json();
-      throw new Error(errorResponse.mensagem || "Falha na autenticação. Verifique suas credenciais e tente novamente.");
+      throw new Error(
+        errorResponse.mensagem ||
+          "Falha na autenticação. Verifique suas credenciais e tente novamente."
+      );
     }
 
     const responseData = await response.json();
@@ -65,12 +64,53 @@ function parseJwt(token) {
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   var jsonPayload = decodeURIComponent(
     atob(base64)
-    .split("")
-    .map(function (c) {
-      return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-    })
-    .join("")
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
   );
 
   return JSON.parse(jsonPayload);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const forgotPasswordForm = document.getElementById("forgotPasswordForm");
+
+  forgotPasswordForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("forgot-email").value;
+
+    try {
+      const response = await fetch(config.api + "/forgot", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        showToast("Um email com as instruções foi enviado.", "success");
+        const modal = bootstrap.Modal.getInstance(
+          document.getElementById("forgotPasswordModal")
+        );
+        modal.hide();
+      } else {
+        const errorText = await response.text();
+        let errorMessage = "Erro desconhecido";
+
+        try {
+          const errorData = JSON.parse(errorText);
+          errorMessage = errorData.message || "Erro desconhecido";
+        } catch (jsonError) {
+          console.error("Erro ao processar JSON de erro:", jsonError);
+        }
+
+        showToast(`Erro: ${errorMessage}`, "error");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar email de recuperação de senha:", error);
+      showToast("Erro ao enviar email. Por favor, tente novamente.", "error");
+    }
+  });
+});
