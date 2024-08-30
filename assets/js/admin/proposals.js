034d0de1b06abda570b4084547873c7df345369a
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-08-29 21:37:51 -0300

    Add pagination on [ADM] - Proposal page

--- assets/js/admin/proposals.js
+++ assets/js/admin/proposals.js
@@ -3,7 +3,6 @@ import showToast from "../app/toast.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const apiUrl = config.api + "/proposal/adm";
-
   let proposals = [];
 
   if (!sessionStorage.getItem("jwt")) {
@@ -15,27 +14,98 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
-  async function loadProposals() {
+  async function loadProposals(query = "", page = 0, size = 9, sort = "") {
     try {
-      const response = await fetch(apiUrl, {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
-        },
-      });
+      const response = await fetch(
+        `${apiUrl}?contain=${encodeURIComponent(
+          query
+        )}&page=${page}&size=${size}&sort=${sort}`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
+          },
+        }
+      );
 
       if (!response.ok) {
         const errorResponse = await response.json();
-        throw new Error(errorResponse.mensagem || "Erro desconhecido");
+        throw new Error(errorResponse.mensagem || "Erro ao carregar dados");
       }
 
-      proposals = await response.json();
+      const result = await response.json();
+      proposals = result.proposals;
+
       displayProposals(proposals);
+      updatePagination(result.currentPage, result.totalPages);
     } catch (error) {
-      console.error("Erro ao obter dados da API: ", error);
       showToast(error.message, "error");
     }
   }
 
+  function updatePagination(currentPage, totalPages) {
+    const paginationContainer = document.getElementById("paginationContainer");
+    paginationContainer.innerHTML = "";
+
+    if (currentPage > 0) {
+      const firstPageButton = document.createElement("li");
+      firstPageButton.className = "page-item";
+      firstPageButton.innerHTML = `<a class="btn app-btn-primary me-2" href="#" aria-label="Primeira">Primeira</a>`;
+      firstPageButton.querySelector("a").addEventListener("click", (e) => {
+        e.preventDefault();
+        loadProposals("", 0);
+      });
+      paginationContainer.appendChild(firstPageButton);
+    }
+
+    if (currentPage > 0) {
+      const prevButton = document.createElement("li");
+      prevButton.className = "page-item";
+      prevButton.innerHTML = `<a class="btn app-btn-primary me-2" href="#" aria-label="Anterior">&laquo;</a>`;
+      prevButton.querySelector("a").addEventListener("click", (e) => {
+        e.preventDefault();
+        loadProposals("", currentPage - 1);
+      });
+      paginationContainer.appendChild(prevButton);
+    }
+
+    const startPage = Math.max(0, currentPage - 1);
+    const endPage = Math.min(totalPages - 1, currentPage + 1);
+
+    for (let i = startPage; i <= endPage; i++) {
+      const pageButton = document.createElement("li");
+      pageButton.className = `page-item ${i === currentPage ? "active" : ""}`;
+      pageButton.innerHTML = `<a class="btn app-btn-primary me-2" href="#">${
+        i + 1
+      }</a>`;
+      pageButton.querySelector("a").addEventListener("click", (e) => {
+        e.preventDefault();
+        loadProposals("", i);
+      });
+      paginationContainer.appendChild(pageButton);
+    }
+
+    if (currentPage < totalPages - 1) {
+      const nextButton = document.createElement("li");
+      nextButton.className = "page-item";
+      nextButton.innerHTML = `<a class="btn app-btn-primary me-2" href="#" aria-label="Próxima">&raquo;</a>`;
+      nextButton.querySelector("a").addEventListener("click", (e) => {
+        e.preventDefault();
+        loadProposals("", currentPage + 1);
+      });
+      paginationContainer.appendChild(nextButton);
+    }
+
+    if (currentPage < totalPages - 1) {
+      const lastPageButton = document.createElement("li");
+      lastPageButton.className = "page-item";
+      lastPageButton.innerHTML = `<a class="btn app-btn-primary" href="#" aria-label="Última">Última</a>`;
+      lastPageButton.querySelector("a").addEventListener("click", (e) => {
+        e.preventDefault();
+        loadProposals("", totalPages - 1);
+      });
+      paginationContainer.appendChild(lastPageButton);
+    }
+  }
   function displayProposals(data) {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
