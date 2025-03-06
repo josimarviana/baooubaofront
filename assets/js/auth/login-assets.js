8e7fc56264176a8a6270675fb31db74db037d4a2
Cristian Alves <cristianalvessilvak@gmail.com>
2025-03-06 11:51:05 -0300

    Atualizando gif homepage para 2025

--- assets/js/auth/login-assets.js
+++ assets/js/auth/login-assets.js
@@ -39,7 +39,7 @@ loginForm.addEventListener("submit", async (event) => {
       const errorResponse = await response.json();
       throw new Error(
         errorResponse.mensagem ||
-          "Falha na autenticação. Verifique suas credenciais e tente novamente."
+        "Falha na autenticação. Verifique suas credenciais e tente novamente."
       );
     }
 
@@ -50,7 +50,7 @@ loginForm.addEventListener("submit", async (event) => {
     const jwt = sessionStorage.getItem("jwt");
     const decodedToken = parseJwt(jwt);
     sessionStorage.setItem("roles", decodedToken.roles.join(", "));
-    
+
     loginForm.reset();
     window.location.href = "../../../pages/logged/home.html";
   } catch (error) {
